7ea9b7e14c381f895c70582461ae77b141193bd9
Kizito007 <nwakakelechi49@gmail.com>
2023-07-22 23:03:39 +0100

    structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 import Header from './layout/Header'
+import Footer from './layout/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -20,6 +21,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   )
